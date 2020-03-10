92c05e688fa6bdc1a4edf3f3b4f94bdeb674407d
Netryle <d.a.n.2004@mail.ru>
2020-03-11 01:38:54 +0700

    Made chessdesk

---
+++ chessdesk.cpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+int desk()
+{
+    string l1, l2, l3, l4, l5, l6, l7, l8, l9;
+    l1 = "8 r n b q k b n r";
+    l2 = "7 p p p p p p p p";
+    l3 = "6                ";
+    l4 = "5                ";
+    l5 = "4                ";
+    l6 = "3                ";
+    l7 = "2 P P P P P P P P";
+    l8 = "1 R N B Q K B N R";
+    l9 = "  a b c d e f g h";
+    cout << l1 << "\n";
+    cout << l2 << "\n";
+    cout << l3 << "\n";
+    cout << l4 << "\n";
+    cout << l5 << "\n";
+    cout << l6 << "\n";
+    cout << l7 << "\n";
+    cout << l8 << "\n";
+    cout << l9 << "\n";
+}
