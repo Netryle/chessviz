f5669ccffcca594389e76a948efc8d5331421d7c
Netryle <d.a.n.2004@mail.ru>
2020-03-11 01:37:24 +0700

    Added output of function desk

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,9 @@
+#include "chessdesk.h"
 #include <iostream>
 using namespace std;
 int main()
-{    
-cout << "Hello";
-cout << "\n"; 
+{
+    cout << "Hello";
+    cout << "\n";
+    cout << desk();
 }
-
